4f79f5b46146260d9bd5b5a3db180fa3bf5eb118
nurbek20 <nurbek19957072@gmail.com>
2022-11-20 22:51:50 +0600

    cors

--- index.js
+++ index.js
@@ -1,53 +1,87 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import fs from 'fs';
 import multer from 'multer';
-import cors from 'cors'
+import cors from 'cors';
+
+import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
-import {UserController, PostController} from './controllers/index.js';
-import {handleValidationErrors, checkAuth} from './utils/index.js';
-// mongodb+srv://admin:wwwwww@cluster0.witomxf.mongodb.net/blog?retryWrites=true&w=majority
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+
+import { UserController, PostController } from './controllers/index.js';
+
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log('DB ok'))
-    .catch((err) => console.log('DB error', err))
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (_, __, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (_, file, cb) => {
-        cb(null, file.originalname);
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
     }
-})
-
-const upload = multer({ storage });
-app.use(express.json());
-app.use(cors());
-app.use('/uploads', express.static('uploads'))
-
-app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)  //? регистрация
-app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)  //? авторизация
-app.get('/auth/me', checkAuth, UserController.getMe)   //? возврашения 
-
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`
-    })
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-app.get('/posts', PostController.getAll);  //? информация всех статья;
-app.get('/posts/:id', PostController.getOne);   //? получения одно статья;
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);   //? создания статья;
-app.delete('/posts/:id', checkAuth, PostController.remove);  //? удаления одного статья;
-app.patch('/posts/:id', checkAuth, handleValidationErrors, PostController.update); //? изменения и обновления одного статя;
+const upload = multer({ storage });
 
-const PORT=process.env.PORT||4444
-app.listen(PORT, (err) => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log(`Сервер запущен на порт${PORT}`);
-})
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
+
+app.listen(process.env.PORT || 8000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK');
+});
+
+
+// const start = async () => {
+//     try {
+//         await mongoose.connect(config.get("dbUrl"), {
+//             useNewUrlParser:true,
+//             useUnifiedTopology:true
+//         })
+
+//         app.listen(PORT, () => {
+//             console.log('Server started on port ', PORT)
+//         })
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+// start()
